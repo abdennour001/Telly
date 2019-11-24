19ca25b3f9af6d05388975cd9c743bedd0e5e983
abdennour <amokranabdennour@gmail.com>
2019-11-24 09:38:48 +0100

    feat: Ajouter la fonctionnalite de creation d'un utilisateur @author AMOKRANE Abdennour
    Adding:
    
     - Methods : UserController@createAccount, UserController@createUser
    
     - Vue: createaccount.jsp
    
     - Additional Models: Reserve, ReserveDao, User, UserDao
    
     - Additional Services: ReserveService, UserService
    
     --fixing-bugs :
    
        * Adding missing models and dao:
    
               -> FormValidationGroup, PersistenceValidationGroup

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -21,6 +21,12 @@ import com.telly.service.UserService;
 @Controller
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	ReserveService reserveService;
+
 	@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
@@ -30,6 +36,30 @@ public class UserController {
 	@RequestMapping("/loggedout")
 	public String showLogout() {
 		return "loggedout";
+	}
+
+	@RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+		
+		model.addAttribute("user", new User());
+		
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+		
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+		
+		return "home";
+
 	}
 	
 	
