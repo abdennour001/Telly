0e92bc5ac79a32c04b94d6affd099b16d3906998
abdennour <amokranabdennour@gmail.com>
2019-11-24 09:18:50 +0100

    feat: Ajouter la fonctionnalite de connextion @author AMOKRANE Abdennour
    Adding files:
    
     - Controller: UserController@showLogin
    
     - Vues: login.jsp, denied.jsp

---
+++ src/com/telly/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.telly.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.telly.dao.FormValidationGroup;
+import com.telly.dao.Reserve;
+import com.telly.dao.User;
+import com.telly.service.ReserveService;
+import com.telly.service.UserService;
+
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+}
